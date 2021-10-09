1fe3a0fb31f53f6d23741f9f515ab2c6400e1b55
tarekh <tarek.hammouda@yahoo.fr>
2021-10-09 05:19:02 +0200

    set READ_DOT_ENV_FILE to true

--- djcrm/settings.py
+++ djcrm/settings.py
@@ -5,7 +5,7 @@ env = environ.Env(
     DEBUG=(bool, False)
 )
 
-READ_DOT_ENV_FILE = env.bool('READ_DOT_ENV_FILE', default=False)
+READ_DOT_ENV_FILE = env.bool('READ_DOT_ENV_FILE', default=True)
 if READ_DOT_ENV_FILE:
     environ.Env.read_env()
 
